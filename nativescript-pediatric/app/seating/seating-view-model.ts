8ee3dbb4f71e90c9707083825623821ea1b3e766
William Emfinger <waemfinger@gmail.com>
2019-10-21 10:24:37 -0500

    fleshing out some code.

--- nativescript-pediatric/app/seating/seating-view-model.ts
+++ nativescript-pediatric/app/seating/seating-view-model.ts
@@ -1,7 +1,58 @@
-import { Observable } from "tns-core-modules/data/observable";
+import { Observable } from 'tns-core-modules/data/observable';
+import * as bluetooth from 'nativescript-bluetooth';
 
 export class SeatingViewModel extends Observable {
+    public static SERVICE_UUID: string = '000012ff-0000-1000-8000-00805f9b34fb';
+
+    public peripheral: bluetooth.Peripheral = null;
+
     constructor() {
         super();
+
+    }
+
+    public async scanAndConnect() {
+        try {
+            const peripheral = await new Promise(async (resolve, reject) => {
+                await bluetooth.startScanning({
+                    serviceUUIDs: [SeatingViewModel.SERVICE_UUID],
+                    seconds: 4,
+                    skipPermissionCheck: false,
+                    onDiscovered: async function(peripheral: bluetooth.Peripheral) {
+                        // stop scanning
+                        await bluetooth.stopScanning();
+                        // resolve
+                        resolve(peripheral);
+                    }
+                });
+                // if we've gotten to here then we've timed out without finding
+                // any devices
+                reject('timeout');
+            }) as bluetooth.Peripheral;
+            // we have a peripheral - so connect to it
+            await new Promise(async (resolve, reject) => {
+                bluetooth.connect({
+                    UUID: peripheral.UUID,
+                    onConnected: function(peripheral: bluetooth.Peripheral) {
+                        this.onConnected(peripheral);
+                        resolve(peripheral);
+                    },
+                    onDisconnected: function(peripheral: bluetooth.Peripheral) {
+                        this.onDisconnected(peripheral);
+                        reject('Could not connect to' + peripheral);
+                    }
+                });
+            });
+            // if we've gotten here then we're connected
+        } catch (err) {
+        }
+    }
+
+    private async onConnected(peripheral: bluetooth.Peripheral) {
+        this.peripheral = peripheral;
+    }
+
+    private async onDisconnected(peripheral: bluetooth.Peripheral) {
+        this.peripheral = peripheral;
     }
 }
