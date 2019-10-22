1f817c8ec61e612b4ad93619e45e4b9711dd6bdb
William Emfinger <waemfinger@gmail.com>
2019-10-22 15:25:40 -0500

    databinding for gauges.

--- nativescript-pediatric/app/driving/driving-view-model.ts
+++ nativescript-pediatric/app/driving/driving-view-model.ts
@@ -1,6 +1,11 @@
 import { Observable } from "tns-core-modules/data/observable";
+import { Prop } from '../obs-prop';
 
 export class DrivingViewModel extends Observable {
+    @Prop() forwardSpeed: number = 75;
+    @Prop() turningSpeed: number = 50;
+    @Prop() acceleration: number = 30;
+
     constructor() {
         super();
     }
